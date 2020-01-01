354525d06c67b169ce46ce622b2c91c02bddf230
Bharatpurohit98 <59228989+Bharatpurohit98@users.noreply.github.com>
2020-01-01 15:51:51 +0530

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Login Form</title>
+<link rel="stylesheet" href="CSS\style.css">
+<head>
+<body style="background-color:#b2bec3">
+<div id="main-wrapper">
+<center><h2>LOGIN FORM</h2>
+<img src="https://cdn1.iconfinder.com/data/icons/avatar-3/512/Manager-512.png" class="avatar"></center>
+<div class="pass"><b>Username:</b><br><input type="text" placeholder="Type..." class="place"></br>
+<b>Password:</b><br><input type="text" placeholder="Type..." class="place" ></br>
+</div>
+<div class="gen"><b>
+<input type="radio" value="male" name="gender" checked required>Male
+<input type="radio" value="female" name="gender" required>Female</b></div>
+<div class="cerf">
+<b>Certifications:</b><select size="1">
+<option>Machine Learning</option>
+<option>Artificial Intelligence</option>
+<option>Cybersecurity</option>
+<option>Web Development</option>
+<option>Data Analytics</option>
+<option>Networking</option>
+<option>Software Development</option>
+</select></div>
+<br><input type="button" value="Login" id="box"></br>
+<br><input type="button" value="Register" id="box1"></br>
+</div>
+</body>
+</html>
